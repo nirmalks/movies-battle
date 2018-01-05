e00b21a24fbc8e1f2ecacc499a1e97b952ab487a
Nirmal Kumar Sekar <kagosekar@gmail.com>
2018-01-05 23:41:53 +0530

    Added input btns

--- src/components/BattlePage.js
+++ src/components/BattlePage.js
@@ -2,14 +2,82 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 
+
+import { withStyles } from 'material-ui/styles';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItem: 'center',
+    justifyContent: 'center'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  }
+});
+
+
 class BattlePage extends Component {
+    state = {
+        movie1Name : "",
+        movie2Name : "" ,
+    }
+    handleMovie1Change = name => event => {
+        this.setState({ movie1Name : event.target.value})
+    }
+
+    handleMovie2Change = name => event => {
+        this.setState({ movie2Name : event.target.value})
+    }
+
+    submitClickHandler = () => {
+        console.log("submit clicked");
+    }
+
     render() {
+        const { classes } = this.props;
         return (
             <div>
                 <Header />
-                <div>hello battle!</div>
+                <h1>Movies Battle!</h1>
+                <form className={classes.container} noValidate autoComplete="off">
+                    <TextField
+                    id="movie1-name"
+                    label="Movie1 Name"
+                    className={classes.textField}
+                    value={this.state.movie1Name}
+                    onChange={this.handleMovie1Change('movie1-name')}
+                    margin="normal"
+                    />
+                    <TextField
+                    id="movie2-name"
+                    label="Movie2 Name"
+                    className={classes.textField}
+                    value={this.state.movie2Name}
+                    onChange={this.handleMovie2Change('movie2-name')}
+                    margin="normal"
+                    />
+              
+                </form>
+                <Button onClick={this.submitClickHandler} raised color="primary" className={classes.button}>
+                Submit
+            </Button>
             </div>
         );
     }
 }
-export default BattlePage;
+
+BattlePage.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(styles)(BattlePage);
