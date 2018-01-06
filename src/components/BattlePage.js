ebc50f8defb7b81312afc6f867bc860d20c59a63
Nirmal Kumar Sekar <kagosekar@gmail.com>
2018-01-06 23:27:02 +0530

    1.Remove unused imports 2.Added handling for response for API Req- movies

--- src/components/BattlePage.js
+++ src/components/BattlePage.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
-
-
 import { withStyles } from 'material-ui/styles';
-import MenuItem from 'material-ui/Menu/MenuItem';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
+import { apiKey } from '../api_key';
 
 const styles = theme => ({
   container: {
@@ -22,7 +20,7 @@ const styles = theme => ({
   },
   button: {
     margin: theme.spacing.unit,
-  }
+  },
 });
 
 
@@ -30,7 +28,12 @@ class BattlePage extends Component {
     state = {
         movie1Name : "",
         movie2Name : "" ,
+        loading: false,
+        movie1Result: [],
+        movie2Result: [],
+        winner :"",
     }
+
     handleMovie1Change = name => event => {
         this.setState({ movie1Name : event.target.value})
     }
@@ -40,19 +43,46 @@ class BattlePage extends Component {
     }
 
     submitClickHandler = () => {
-        console.log("submit clicked");
+        let movie1Data = fetch(`https://api.themoviedb.org/3/find/${this.state.movie1Name}?api_key=${apiKey}&external_source=imdb_id`).then(
+          response => {
+            response.json().then(data => {
+              this.setState({ movie1Result: data.movie_results[0] });
+            });
+          }
+        );
+
+        let movie2Data = fetch(`https://api.themoviedb.org/3/find/${this.state.movie2Name}?api_key=${apiKey}&external_source=imdb_id`).then(
+          response => {
+            response.json().then(data => {
+              this.setState({ movie2Result: data.movie_results[0] });
+            });
+          }
+        );
+
+        Promise.all([movie1Data , movie2Data]).then( () => {
+            console.log(this.state.movie1Result);
+            console.log(this.state.movie2Result);
+            if(this.state.movie1Result.vote_average > this.state.movie2Result.vote_average) {
+              this.setState({winner : "movie1"});
+            } else if (this.state.movie1Result.vote_average === this.state.movie2Result.vote_average) {
+              this.setState({winner : "tie"});
+            } else {
+              this.setState({winner : "movie2"});
+            }
+        }
+        );
     }
 
     render() {
         const { classes } = this.props;
         return (
-            <div>
+            <div >
                 <Header />
                 <h1>Movies Battle!</h1>
                 <form className={classes.container} noValidate autoComplete="off">
                     <TextField
                     id="movie1-name"
-                    label="Movie1 Name"
+                    label="Enter an IMDB ID"
                     className={classes.textField}
                     value={this.state.movie1Name}
                     onChange={this.handleMovie1Change('movie1-name')}
@@ -60,7 +90,7 @@ class BattlePage extends Component {
                     />
                     <TextField
                     id="movie2-name"
-                    label="Movie2 Name"
+                    label="Enter an IMDB ID"
                     className={classes.textField}
                     value={this.state.movie2Name}
                     onChange={this.handleMovie2Change('movie2-name')}
@@ -70,7 +100,7 @@ class BattlePage extends Component {
                 </form>
                 <Button onClick={this.submitClickHandler} raised color="primary" className={classes.button}>
                 Submit
-            </Button>
+                </Button>            
             </div>
         );
     }
