4f362c5c6801c6a3fd07b33f825dad6e14d02aaa
Nirmal Kumar Sekar <kagosekar@gmail.com>
2018-01-07 15:41:05 +0530

    Added nav links using react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,16 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import { NavLink } from 'react-router-dom';
 
 const styles = theme => ({
   root: {
     width: '100%',
+  },
+  button: {
+    marginLeft:10,
+    textDecoration:'none'
   },
 });
 
@@ -17,9 +23,26 @@ const Header  = (props) => {
     <div className={classes.root}>
       <AppBar position="static" color="default">
         <Toolbar>
-          <Typography type="title" color="inherit">
-            Favourite Movies
+          <Typography type="title" color="inherit" >
+          <NavLink
+          exact
+          to="/"
+          activeStyle={{
+            fontWeight: 'bold',
+            color: '#3f51b5',
+            textDecoration:'underline',
+          }}
+        >Favourite Movies</NavLink>
           </Typography>
+          <Button className={classes.button} color="inherit"><NavLink
+          exact
+          to="/battle"
+          activeStyle={{
+            fontWeight: 'bold',
+            color: '#3f51b5',
+            textDecoration:'underline',
+          }}
+        >Battle</NavLink></Button>
         </Toolbar>
       </AppBar>
     </div>
